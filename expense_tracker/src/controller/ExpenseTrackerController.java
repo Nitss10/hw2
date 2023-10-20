7e8698ec4203caf3c6ac709e5e4ff444968e322c
Niti Mangwani <nitimangwani@1x-nat-vl931-172-30-143-163.wireless.umass.edu>
2023-10-20 18:13:48 -0400

    filterTransactions method added in ExpenseTrackerController which will have validation checks on inputs for filtering and would initialise attribute value based on the selected attribute and eventually call the corresponding filter method to fetch filtered rows indices

--- expense_tracker/src/controller/ExpenseTrackerController.java
+++ expense_tracker/src/controller/ExpenseTrackerController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import filter.AmountFilter;
+import filter.CategoryFilter;
+import filter.TransactionFilter;
 import view.ExpenseTrackerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,6 +16,7 @@ public class ExpenseTrackerController {
   
   private ExpenseTrackerModel model;
   private ExpenseTrackerView view;
+
 
   public ExpenseTrackerController(ExpenseTrackerModel model, ExpenseTrackerView view) {
     this.model = model;
@@ -41,6 +46,30 @@ public class ExpenseTrackerController {
     Transaction t = new Transaction(amount, category);
     model.addTransaction(t);
     view.getTableModel().addRow(new Object[]{t.getAmount(), t.getCategory(), t.getTimestamp()});
+    refresh();
+    return true;
+  }
+
+  public boolean filterTransactions(String attribute , String attributeValue) {
+
+    TransactionFilter filter = null;
+    if(attribute == "Category")
+    {
+      if (!InputValidation.isValidCategory(attributeValue)) {
+        return false;
+      }
+      filter = new CategoryFilter(attributeValue);
+    }
+    else if(attribute == "Amount")
+    {
+      double amountDouble = Double.parseDouble(attributeValue);
+      if (!InputValidation.isValidAmount(amountDouble)) {
+        return false;
+      }
+      filter = new AmountFilter(amountDouble);
+    }
+    List<Integer> selectedRows  = filter.filter(model.getTransactions());
+    view.setSelectedRows(selectedRows);
     refresh();
     return true;
   }
